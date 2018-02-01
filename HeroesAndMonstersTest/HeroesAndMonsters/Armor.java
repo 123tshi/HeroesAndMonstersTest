743f5ef7df0f3527c5a37ac6ad17e755c99c07da
mng <mng@student.smusd.us>
2018-02-01 13:59:40 -0800

    oop

--- HeroesAndMonstersTest/HeroesAndMonsters/Armor.java
+++
@@ -1,8 +0,0 @@
-public class Armor extends Item{
-    public Armor(){
-        super();
-    }
-    public String toString(){
-        return "The armor is located at " + super.toString() + ".";
-    }
-}
