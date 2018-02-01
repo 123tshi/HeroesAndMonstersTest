b0a6181c73f0f9639bb6e2bf485b660ed845f745
ts <123tshi@gmail.com>
2018-02-01 13:34:02 -0800

    First Commit

--- HeroesAndMonstersRedux/Bomb.java
+++
@@ -1,9 +0,0 @@
-public class Bomb extends Item{
-    public Bomb(){
-        super();
-    }
-    
-    public String toString(){
-        return "The bomb is located at " + super.toString();
-    }
-}
--- HeroesAndMonstersRedux/Satchel.java
+++
@@ -1,9 +0,0 @@
-public class Satchel extends Item{
-    public Satchel(){
-        super();
-    }
-    
-    public String toString(){
-        return "The satchel is located at " + super.toString() + ".";
-    }
-}
